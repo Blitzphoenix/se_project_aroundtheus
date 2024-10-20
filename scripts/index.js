9a517b6ea89caf3fd30f308361b90d211fa5341b
Nimai <nimaibarua1@gmail.com>
2024-10-20 15:30:51 -0400

    project stage 4 - 3

--- scripts/index.js
+++ scripts/index.js
@@ -23,25 +23,25 @@ const card6= {
   link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   }
 
-let initialCards = [card1 , card2 , card3 , card4 , card5 , card6]
+const initialCards = [card1 , card2 , card3 , card4 , card5 , card6]
 
 
 const editButton = document.querySelector("#edit-button");
 const closeButton = document.querySelector("#close-button");
-const model = document.querySelector(".model");
-const modelForm = model.querySelector(".model__form");
+const modal = document.querySelector(".modal");
+const modalForm = modal.querySelector(".modal__form");
 
 const profileName= document.querySelector(".profile__name")
 const profileDescription= document.querySelector(".profile__description")
-const editProfileName= document.querySelector(".model__form_name")
-const editProfileDescription= document.querySelector(".model__form_description")
+const editProfileName= document.querySelector("#name")
+const editProfileDescription= document.querySelector("#description")
 
 const cardTemplate = document.querySelector("#card-template").content.firstElementChild
 const cardList = document.querySelector(".cards__list")
 
 
 function closePopup(){
-  model.classList.remove("model__open")
+  modal.classList.remove("modal_open")
 }
 
 function getCardElement(cardData){
@@ -51,7 +51,7 @@ function getCardElement(cardData){
 
   cardTitleEl.textContent = cardData.name
   cardImageEl.src = cardData.link
-
+  cardImageEl.alt = cardData.name
   return cardElement
 }
 
@@ -60,11 +60,11 @@ function getCardElement(cardData){
 editButton.addEventListener("click", () => {
   editProfileName.value=profileName.textContent
   editProfileDescription.value=profileDescription.textContent
-  model.classList.add("model__open")
+  modal.classList.add("modal_open")
 })
 
 closeButton.addEventListener("click", closePopup )
-modelForm.addEventListener("submit", (e) => {
+modalForm.addEventListener("submit", (e) => {
   e.preventDefault();
 profileName.textContent=editProfileName.value
 profileDescription.textContent= editProfileDescription.value
